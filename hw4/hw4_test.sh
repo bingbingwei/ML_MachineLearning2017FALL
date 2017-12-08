7298697f654f0da1c7219a55e721119631b9a09a
bingbingwei <b03902093@ntu.edu.tw>
2017-12-08 20:00:20 +0800

    model upload

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,1 +1,3 @@
-python3 test.py --model_data model_semi.h5 --sub_data $1
+wget https://www.dropbox.com/s/czi5gkluofwtqoo/model.h5?dl=1
+mv model.h5?dl=1 model.h5
+python3 test.py --model_data model.h5 --sub_data $1
